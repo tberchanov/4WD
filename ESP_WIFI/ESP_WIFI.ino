0d98d4d434fa3484936dc9a5056b5c61b4f44b13
Anatolii Berchanov <tberchanov@gmail.com>
2023-11-04 17:09:54 +0200

    Move sensitive constants to ESP_WIFI/Constants.h

--- ESP_WIFI/ESP_WIFI.ino
+++ ESP_WIFI/ESP_WIFI.ino
@@ -1,34 +1,61 @@
 
 
-#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
 #include <Arduino.h>
+#include "Constants.h"
 
 #define ALARM_Pin 12
 
-const char* ssid     = "YOUR_WIFI_NAME";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "YOUR_WIFI_PASSWORD";     // The password of the Wi-Fi network
+const char* ssid     = WIFI_SSID;         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = WIFI_PASSWORD;     // The password of the Wi-Fi network
 
 unsigned long checkAlarmDelay = 1000;
 unsigned long lastCheckedAlarmTime = 0;
 
 HTTPClient http;
 
-String serverPath = "https://api.telegram.org/<YOUR_BOT_ID>/sendMessage";
+String serverPath = TELEGRAM_BOT_SEND_MESSAGE_URL;
 
 // Root certificate
-const char IRG_Root_X1 [] PROGMEM = R"CERT(
-YOUR_ROOT_CERTIFICATE
+const char IRG_Root_X1[] PROGMEM = R"CERT(
+-----BEGIN CERTIFICATE-----
+MIIDxTCCAq2gAwIBAgIBADANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx
+EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT
+EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp
+ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTA5MDkwMTAwMDAwMFoXDTM3MTIzMTIz
+NTk1OVowgYMxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH
+EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjExMC8GA1UE
+AxMoR28gRGFkZHkgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkgLSBHMjCCASIw
+DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAL9xYgjx+lk09xvJGKP3gElY6SKD
+E6bFIEMBO4Tx5oVJnyfq9oQbTqC023CYxzIBsQU+B07u9PpPL1kwIuerGVZr4oAH
+/PMWdYA5UXvl+TW2dE6pjYIT5LY/qQOD+qK+ihVqf94Lw7YZFAXK6sOoBJQ7Rnwy
+DfMAZiLIjWltNowRGLfTshxgtDj6AozO091GB94KPutdfMh8+7ArU6SSYmlRJQVh
+GkSBjCypQ5Yj36w6gZoOKcUcqeldHraenjAKOc7xiID7S13MMuyFYkMlNAJWJwGR
+tDtwKj9useiciAF9n9T521NtYJ2/LOdYq7hfRvzOxBsDPAnrSTFcaUaz4EcCAwEA
+AaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYwHQYDVR0OBBYE
+FDqahQcQZyi27/a9BUFuIMGU2g/eMA0GCSqGSIb3DQEBCwUAA4IBAQCZ21151fmX
+WWcDYfF+OwYxdS2hII5PZYe096acvNjpL9DbWu7PdIxztDhC2gV7+AJ1uP2lsdeu
+9tfeE8tTEH6KRtGX+rcuKxGrkLAngPnon1rpN5+r5N9ss4UXnT3ZJE95kTXWXwTr
+gIOrmgIttRD02JDHBHNA7XIloKmf7J6raBKZV8aPEjoJpL1E/QYVN8Gb5DKj7Tjo
+2GTzLH4U/ALqn83/B2gX2yKQOC16jdFU8WnjXzPKej17CuPKf1855eJ1usV2GDPO
+LPAvTK33sefOT6jEm0pUBsV/fdUID+Ic/n4XuKxe9tQWskMJDE32p2u0mYRlynqI
+4uJEvlz36hz1
+-----END CERTIFICATE-----
 )CERT";
 
 X509List cert(IRG_Root_X1);
 
 void setup() {
-  pinMode(ALARM_Pin,INPUT);
   Serial.begin(9600);         // Start the Serial communication to send messages to the computer
+
+  pinMode(ALARM_Pin,INPUT);
+  
   delay(10);
   Serial.println('\n');
+
+  Serial.println(WIFI_SSID);
   
   WiFi.begin(ssid, password);             // Connect to the network
   Serial.print("Connecting to ");
@@ -80,7 +107,7 @@ void loop() {
         http.addHeader("content-type", "application/x-www-form-urlencoded");
         http.addHeader("accept", "*/*");
 
-        int httpResponseCode = http.POST("chat_id=<YOUR_CHAT_ID>&text=Movement is detected!");
+        int httpResponseCode = http.POST(TELEGRAM_BOT_BODY);
         
         if (httpResponseCode > 0) {
           Serial.print("HTTP Response code: ");
